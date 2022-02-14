c92fa7c8c4456cd8b1e7ea219e5c20b777a8d896
Miguel <miguelarrieta02@gmail.com>
2022-02-14 11:24:13 +0000

    Added methods to tell if a number is positive

--- src/main/java/com/bus_system/util/OptionalOps.java
+++ src/main/java/com/bus_system/util/OptionalOps.java
@@ -1,9 +1,18 @@
 package com.bus_system.util;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public class OptionalOps {
     public static <T> String getStringValue(Optional<T> value) {
         return value.map(Object::toString).orElse("N/A");
+    }
+
+    public static boolean isPositive(Optional<Number> number) {
+        return Math.signum(number.orElse(0).doubleValue()) >= 0;
+    }
+
+    public static boolean areAllPositive(Optional<Number>... numbers) {
+        return Arrays.stream(numbers).allMatch(OptionalOps::isPositive);
     }
 }

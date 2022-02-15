a30ef12f255636acf55da1dde6050ab98239edf9
Miguel Bloggs <arrietam@tcd.ie>
2022-02-15 09:37:05 +0000

    Cleaned up Graph.addEdge and added toString method

--- src/main/java/com/bus_system/util/graph/Graph.java
+++ src/main/java/com/bus_system/util/graph/Graph.java
@@ -1,17 +1,18 @@
 package com.bus_system.util.graph;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Graph<E> {
     private final Map<E, Set<Edge<E>>> adjacencyMap = new HashMap<>();
 
     public void addEdge(E source, E destination, double weight) {
-        adjacencyMap.compute(source, (k, v) -> {
-            if (v == null) {
-                v = new HashSet<>();
-            }
-            v.add(new Edge<>(source, destination, weight));
-            return v;
-        });
+        adjacencyMap.computeIfAbsent(source, v -> new HashSet<>()).add(new Edge<>(source, destination, weight));
+    }
+
+    @Override
+    public String toString() {
+        return adjacencyMap.entrySet().stream().map(e -> e.getKey().toString() + " -> " + e.getValue()).
+                collect(Collectors.joining("\n"));
     }
 }

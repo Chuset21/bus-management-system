ad8ba00739ecad3fd26b12883b9b83432c3f4752
Miguel <miguelarrieta02@gmail.com>
2022-02-16 14:08:08 +0000

    Added TestGraph.testGetShortestPath

--- src/test/java/TestGraph.java
+++ src/test/java/TestGraph.java
@@ -1,8 +1,11 @@
 import com.bus_system.util.graph.Graph;
+import com.bus_system.util.path.Path;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -35,5 +38,53 @@ public class TestGraph {
         graph.addEdge('A', 'C', 1);
 
         assertEquals(new HashSet<>(List.of('A', 'B', 'C')), graph.getVertexSet());
+    }
+
+    @Test
+    public void testGetShortestPath() {
+        final Graph<Integer> graph = new Graph<>();
+        graph.addEdge(2, 0, 7);
+        graph.addEdge(2, 1, 3);
+        graph.addEdge(0, 1, 9);
+        graph.addEdge(1, 3, 5);
+        graph.addEdge(1, 6, 7);
+        graph.addEdge(3, 5, 4);
+        graph.addEdge(3, 7, 5);
+        graph.addEdge(5, 7, 4);
+        graph.addEdge(7, 6, 2);
+        graph.addEdge(4, 7, 3);
+        graph.addEdge(4, 2, 8);
+
+        Path<Integer> path = graph.getShortestPath(2, 6);
+
+        List<Integer> expectedPath = new LinkedList<>();
+        expectedPath.add(2);
+        expectedPath.add(1);
+        expectedPath.add(6);
+
+        assertEquals(expectedPath, path.getPathTaken());
+        assertEquals(10, path.getTotalCost().orElse(-1d));
+
+        path = graph.getShortestPath(4, 6);
+
+        expectedPath.clear();
+        expectedPath.add(4);
+        expectedPath.add(7);
+        expectedPath.add(6);
+
+        assertEquals(expectedPath, path.getPathTaken());
+        assertEquals(5, path.getTotalCost().orElse(-1d));
+
+        path = graph.getShortestPath(5, 1);
+
+        expectedPath.clear();
+
+        assertEquals(expectedPath, path.getPathTaken());
+        assertEquals(Optional.empty(), path.getTotalCost());
+
+        path = graph.getShortestPath(10, 1);
+
+        assertEquals(expectedPath, path.getPathTaken());
+        assertEquals(Optional.empty(), path.getTotalCost());
     }
 }

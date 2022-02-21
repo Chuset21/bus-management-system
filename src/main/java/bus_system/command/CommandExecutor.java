7670322e3e57736ad567e7c64360dc89cb196261
Miguel <miguelarrieta02@gmail.com>
2022-02-21 20:35:33 +0000

    Started implementing a way to handle errors

--- src/main/java/bus_system/command/CommandExecutor.java
+++ src/main/java/bus_system/command/CommandExecutor.java
@@ -13,18 +13,28 @@ public final class CommandExecutor {
     }
 
     private static final Map<String, Command> COMMANDS;
-//    private static final ErrorCommand ERROR = new ErrorCommand();
 
     static {
         COMMANDS = new HashMap<>();
-        Help.ALIASES.forEach(a -> COMMANDS.put(a, new Help()));
+
+        final Help help = new Help();
+        Help.ALIASES.forEach(a -> COMMANDS.put(a, help));
     }
 
     public static Map<String, Command> getCommands() {
         return Collections.unmodifiableMap(COMMANDS);
     }
 
-//    public static void execute(String command, String... args) {
-//        COMMANDS.getOrDefault(command, ERROR).execute(args);
-//    }
+    public static void execute(String commandName, String... args) {
+        final Command command = COMMANDS.get(commandName);
+        if (command == null) {
+            System.err.println("Please provide a valid command.\nFor more information try '--help'.");
+        } else {
+            try {
+                command.execute(args);
+            } catch (Exception e) {  // This is how errors will be handled
+                System.err.println(e.getMessage());
+            }
+        }
+    }
 }

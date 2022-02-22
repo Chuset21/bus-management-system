7d91b5bbd265d95722827526c02498bed79ce1ab
Miguel <miguelarrieta02@gmail.com>
2022-02-22 21:33:48 +0000

    Added skeleton of ShortestPath command

---
+++ src/main/java/bus_system/command/commands/ShortestPath.java
@@ -0,0 +1,19 @@
+package bus_system.command.commands;
+
+import bus_system.command.Command;
+
+import java.util.List;
+
+public final class ShortestPath implements Command {
+    public final static List<String> ALIASES = List.of("-sp", "--shortest-path");
+
+    @Override
+    public void execute(String... strings) {
+
+    }
+
+    @Override
+    public String getDescription() {
+        return "Shortest path description";
+    }
+}

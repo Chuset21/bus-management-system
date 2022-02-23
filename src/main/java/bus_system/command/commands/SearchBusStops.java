26e4c9a49b4428cd2cd1e0741bb3c5353df6cd21
Miguel <miguelarrieta02@gmail.com>
2022-02-23 15:23:11 +0000

    Implemented SearchBusStops.getDescription

--- src/main/java/bus_system/command/commands/SearchBusStops.java
+++ src/main/java/bus_system/command/commands/SearchBusStops.java
@@ -1,6 +1,7 @@
 package bus_system.command.commands;
 
 import bus_system.command.Command;
+import bus_system.command.ansi.ConsoleColor;
 
 import java.util.List;
 
@@ -15,6 +16,20 @@ public class SearchBusStops implements Command {
 
     @Override
     public String getDescription() {
-        return "Description of Search Bus Stops"; // TODO Implement
+        return """
+                    search bus stops    %s      %s
+                        Optional arguments:  %s
+                \t\tExamples:
+                            Full Name:      %s
+                            Partial Name:   %s
+                            Empty Name:     %s""".formatted(
+                ConsoleColor.colorize(ConsoleColor.YELLOW_BOLD, ALIASES.toString()),
+                ConsoleColor.colorize(ConsoleColor.CYAN,
+                        "Searches for a bus stop given a starting portion of the name, ignores casing."),
+                ConsoleColor.colorize(ConsoleColor.PURPLE_BRIGHT, "<stop name portion>"),
+                ConsoleColor.colorize(ConsoleColor.BLUE_BRIGHT, ALIASES.get(ALIASES.size() - 1) + " \"FLAGSTOP SB ON WESTHILL DR\""),
+                ConsoleColor.colorize(ConsoleColor.BLUE_BRIGHT, ALIASES.get(ALIASES.size() - 1) + " WATER"),
+                ConsoleColor.colorize(ConsoleColor.BLUE_BRIGHT, ALIASES.get(ALIASES.size() - 1))
+        );
     }
 }

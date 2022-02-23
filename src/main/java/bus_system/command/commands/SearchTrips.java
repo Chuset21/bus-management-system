c55bbe168e71fe593ef1581d561eafeefe518822
Miguel <miguelarrieta02@gmail.com>
2022-02-23 16:04:20 +0000

    Implemented SearchTrips.getDescription

--- src/main/java/bus_system/command/commands/SearchTrips.java
+++ src/main/java/bus_system/command/commands/SearchTrips.java
@@ -1,6 +1,7 @@
 package bus_system.command.commands;
 
 import bus_system.command.Command;
+import bus_system.command.ansi.ConsoleColor;
 
 import java.util.List;
 
@@ -9,12 +10,26 @@ public final class SearchTrips implements Command {
 
     @Override
     public int execute(String... strings) {  // TODO Implement
+        if (strings.length != 1) {
+            throw new IllegalArgumentException("Please provide one argument in the form 'hh:mm:ss'");
+        }
+
+
         return 0;
     }
 
 
     @Override
     public String getDescription() {
-        return "Description of Search Trip"; // TODO Implement
+        return """
+                    search trips    %s      %s
+                        Arguments:  %s
+                \t\tExample:    %s""".formatted(
+                ConsoleColor.colorize(ConsoleColor.YELLOW_BOLD, ALIASES.toString()),
+                ConsoleColor.colorize(ConsoleColor.CYAN,
+                        "Searches for all trips with a given arrival time, returning full details of all trips matching the criteria."),
+                ConsoleColor.colorize(ConsoleColor.PURPLE_BRIGHT, "<arrival time> in the format 'hh:mm:ss'"),
+                ConsoleColor.colorize(ConsoleColor.BLUE_BRIGHT, ALIASES.get(ALIASES.size() - 1) + " 23:23:02")
+        );
     }
 }

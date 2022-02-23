beff0d6081616b6f89744b0ef2fc30fed919d7ae
Miguel <miguelarrieta02@gmail.com>
2022-02-23 14:02:03 +0000

    Slightly changed layout

--- src/main/java/bus_system/Main.java
+++ src/main/java/bus_system/Main.java
@@ -8,19 +8,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    private static final Scanner SCANNER = new Scanner(System.in);
-
     public static void main(String[] args) {
         System.out.println(ConsoleColor.colorize(ConsoleColor.WHITE_BRIGHT,
                 "Loading application, please wait and don't input anything as your inputs will be buffered.\n"));
         CommandExecutor.execute("help");
 
+        final Scanner scanner = new Scanner(System.in);
         int returnCode;
         do {
-            final List<String> tokens = Arrays.stream(SCANNER.nextLine().split("[\s\t]+")).map(String::trim).toList();
+            final List<String> tokens = Arrays.stream(scanner.nextLine().split("[\s\t]+")).map(String::trim).toList();
             returnCode = CommandExecutor.execute(tokens.get(0), tokens.subList(1, tokens.size()).toArray(new String[0]));
         } while (returnCode == 0);
 
-        SCANNER.close();
+        scanner.close();
     }
 }

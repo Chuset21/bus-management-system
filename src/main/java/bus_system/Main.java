43bb37ad004b1e21e3029d47a9b5271315f02b7d
Miguel <miguelarrieta02@gmail.com>
2022-02-25 10:50:57 +0000

    Removed unnecessary code

--- src/main/java/bus_system/Main.java
+++ src/main/java/bus_system/Main.java
@@ -7,25 +7,8 @@ import java.util.Scanner;
 
 public final class Main {
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String RAINBOW_MESSAGE;
-
-    static {
-        final StringBuilder stringBuilder = new StringBuilder();
-        final String rawText = "Your console supports ansi colors";
-
-        for (int i = 0; i < rawText.length(); i++) {
-            stringBuilder.append("\033[0;9%dm".formatted(i % 6 + 1)).append(rawText.charAt(i));
-        }
-
-        RAINBOW_MESSAGE = stringBuilder.append(ConsoleColor.RESET.getValue()).toString();
-    }
 
     public static void main(String[] args) {
-//        System.out.printf("""
-//                %s
-//                If the above text has no funny characters and you'd like colors please input 'y', input anything else otherwise:\040""",
-//                RAINBOW_MESSAGE);
-//        ConsoleColor.supportsAnsi = "y".equalsIgnoreCase(SCANNER.nextLine().trim());
         System.out.println(ConsoleColor.colorize(ConsoleColor.WHITE_BRIGHT,
                 "\nLoading application, please wait and don't input anything as your inputs will be buffered.\n"));
         CommandExecutor.execute("help");
